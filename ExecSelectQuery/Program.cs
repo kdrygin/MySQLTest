84f5561e81ee4ead2f00f4ef7cb795455ae66cd5
Дрыгин Константин <kdrygin@gmail.com>
2021-10-12 13:54:38 -0700

    Add project files.

---
+++ ExecSelectQuery/Program.cs
@@ -0,0 +1,41 @@
+﻿// MySQL Select Query Test
+// https://dev.mysql.com/doc/connector-net/en/connector-net-tutorials-sql-command.html
+
+using System;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySQLTest
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string connStr = "server=192.168.0.13;user=dky;database=test;port=3306;password=123";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "SELECT * FROM authors ";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader res = cmd.ExecuteReader();
+
+                while (res.Read())
+                {
+                    Console.WriteLine($"{res[1]} {res[2]} {res[3]}");
+                }
+                res.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+    }
+}
---
+++ TestSchemaConnection/Program.cs
@@ -0,0 +1,47 @@
+﻿// MySQL Connection Test
+// https://dev.mysql.com/doc/connector-net/en/connector-net-programming-getschema.html
+//
+
+using System;
+using System.Data;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySQLTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connStr = "server=192.168.0.13;user=dky;database=test;port=3306;password=123";
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                //DataTable table = conn.GetSchema("MetaDataCollections");
+                DataTable table = conn.GetSchema("Tables");
+
+                foreach (System.Data.DataRow row in table.Rows)
+                {
+                    foreach (System.Data.DataColumn col in table.Columns)
+                    {
+                        Console.WriteLine("{0} = {1}", col.ColumnName, row[col]);
+                    }
+                    Console.WriteLine("============================");
+                }
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("Done.");
+        }
+    }
+}
