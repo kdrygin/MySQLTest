635d89c1d8bf1c4dc8910181156ad754569216f6
Дрыгин Константин <kdrygin@gmail.com>
2021-10-15 23:21:24 -0700

    Random e-mail generation in Insert Query

--- ExecInsertQuery/Program.cs
+++ ExecInsertQuery/Program.cs
@@ -14,7 +14,7 @@ namespace MySQLTest
         {
             string connStr = "server=10.0.4.94;user=dky;database=test;port=3306;password=1qaz2wsx";
             //string connStr = "server=192.168.0.13;user=dky;database=test;port=3306;password=123";
-            
+
             MySqlConnection conn = new MySqlConnection(connStr);
             try
             {
@@ -22,7 +22,7 @@ namespace MySQLTest
                 conn.Open();
 
                 string sql = "INSERT INTO `authors` (`id`, `first_name`, `last_name`, `email`, `birthdate`, `added`) " +
-                             "VALUES (0, 'Test', 'Test', 'test1@test.org', '2000-01-01', '2020-01-01 00:00:00');";
+                             $"VALUES (0, 'Test', 'Test', '{RandomString()}@test.org', '2000-01-01', '2020-01-01 00:00:00');";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 cmd.ExecuteNonQuery();
             }
@@ -33,6 +33,16 @@ namespace MySQLTest
 
             conn.Close();
             Console.WriteLine("Done.");
+        }
+
+        static string RandomString()
+        {
+            //https://stackoverflow.com/questions/730268/unique-random-string-generation
+            Guid g = Guid.NewGuid();
+            string GuidString = Convert.ToBase64String(g.ToByteArray());
+            GuidString = GuidString.Replace("=", "");
+            GuidString = GuidString.Replace("+", "");
+            return GuidString;
         }
     }
 }
